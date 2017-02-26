3a6a40daa5d39370dd48221e21fa4df77a2f0e18
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-02-25 16:59:39 -0800

    adjust js text to fix syntax errors, as the input ended unexpectedly due to an unclose { and ( in the file.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,6 @@ var add = function(number1, number2, number3, number4, number5) {
   return number1 + number2 + number3 + number4 + number5;
 };
 
-$(document).ready(function() {
-  $(".clickable").click(function() {
-  $("#ethiopia-hidden").toggle();
-  $("#ethiopia-showing").toggle();
-});
-
-$("#ethiopia-click").click(function() {
-    $("img").toggleClass("ethiopia-image");
-});
-
 // Everything below this line is user interface (or front-end) logic:
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
@@ -26,17 +16,17 @@ $(document).ready(function() {
 
    var result = add(size, drink, coffee, flavor, time);
 
-   if (result >=5 && <=9) {
+   if (result >=5 && result <=9) {
      $("#ethiopia").hide();
      $("#honduras").hide();
      $("#vietnam").hide();
      $("#hawaii").show();
-   } else if (result >=10 && <=14) {
+   } else if (result >=10 && result <=14) {
      $("#hawaii").hide();
      $("#honduras").hide();
      $("#vietnam").hide();
      $("#ethiopia").show();
-   } else if (result >=15 && <=18) {
+   } else if (result >=15 && result <=18) {
      $("#hawaii").hide();
      $("#ethiopia").hide();
      $("#vietnam").hide();
@@ -47,5 +37,5 @@ $(document).ready(function() {
      $("#honduras").hide();
      $("#vietnam").show();
    }
-
+ });
 });
